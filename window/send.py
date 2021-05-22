abef86230472632ee346cc48c90fea209c613002
parkjnseok <jseg1225@naver.com>
2021-05-22 19:20:36 +0900

    send수정
    수정

--- window/send.py
+++ window/send.py
@@ -2,7 +2,7 @@ import requests
 import hashlib
 
 def file():
-    f = open("D:/study/network.txt", 'rb')
+    f = open("D:/study/network.txt", 'rb')      #파일위치지정 필요
     data = f.read()
     sha256 = hashlib.sha256(data).hexdigest()
     sha512 = hashlib.sha512(data).hexdigest()
@@ -10,7 +10,14 @@ def file():
     hash = {'sha256': sha256,'sha512': sha512, 'md5':md5}
     f.close()
     #add(hash)
-    search(hash)
+    a = search(hash)
+    if a == 2:
+        return 2            # db에 해쉬값이 존재한다
+    elif a == 1:
+        return 1
+    else:
+        print("error")
+
     # return hash
 
 def search(hash):
@@ -21,18 +28,25 @@ def search(hash):
     res = requests.post(url, json=data)
     flag = res.text
     if flag == "2":         #이게 db에 값이 있는경우
-        print (res.text)
+        return 2
+    elif flag == "1":
+        return 1
     else:
-        print("a")
+        print("error")
 
 def add(hash):
     # hash = file()
     # print(hash)
     url=f'http://35.233.216.2:5000/add'
+    #파일이름, 파일형식 정보 받아와야됨(name, extension)
     data = {'name' : 'test', 'sha256' : hash['sha256'], 'sha512' : hash['sha512'], 'md5' : hash['md5'], 'extension' : 'txt'}
     res = requests.post(url, json=data)
     flag = res.text
-    print(flag)
+    if flag == "Insert Success":
+        return 2
+    elif flag == "Insert Fail":
+        return 1
+
 
 file()
 
